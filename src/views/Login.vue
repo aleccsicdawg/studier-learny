e7fce7b97ae39817543ad67b5b59021cc08e88f8
aleccsicdawg <guccimanelspdk@gmail.com>
2018-11-07 23:19:06 +0200

    First commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,2 @@
-<template v-if="loginType === 'username'">
-  <label>Имя пользователя</label>
-  <input placeholder="Введите имя пользователя">
-</template>
-<template v-else>
-  <label>Email</label>
-  <input placeholder="Введите адрес email">
+<template>
 </template>
